def1ed9a46b574151df4a16413afa998ad5c6e6e
Yuki Kobayashi <kobayashi.yuki@gmail.com>
2018-06-18 23:59:08 +0900

    Initial version. (tentative)

---
+++ FolderAnalyzer/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace FolderAnalyzer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(733, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(261, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(534, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(21, 88);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(852, 26);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(876, 88);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 34);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(21, 148);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(954, 505);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1006, 693);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ FolderAnalyzer/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// http://tinqwill.blog59.fc2.com/blog-entry-84.html
+using Shell32;
+using SHDocVw;
+using System.IO;
+
+namespace FolderAnalyzer
+{
+    public partial class Form1 : Form
+    {
+        private Timer m_timer = new Timer();
+        private int m_interval = 1000; // ms
+        private bool m_bLogging = true; // logging or not
+
+        private int m_elapsed = 0;
+
+        private string m_curpaths; // currently opened paths in explorer windows
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            m_timer.Tick += new EventHandler(MyCallback);
+            m_timer.Interval = m_interval;
+            m_timer.Enabled = true;
+
+            listView1.View = View.Details;
+            listView1.FullRowSelect = true;
+            listView1.GridLines = true;
+
+            ColumnHeader chpath = new ColumnHeader();
+            chpath.Text = "Path";
+            chpath.Width = 500;
+            ColumnHeader chval = new ColumnHeader();
+            chval.Text = "Value";
+            chval.Width = 100;
+            ColumnHeader[] charray = { chpath, chval };
+            listView1.Columns.AddRange(charray);
+
+        }
+
+        public void MyCallback(object sender, EventArgs e)
+        {
+            m_elapsed += m_interval;
+            label2.Text = m_elapsed.ToString();
+
+            label1.Text = "";
+
+            string paths = "";
+
+            Shell shell = new Shell();
+            ShellWindows win = shell.Windows();
+            foreach (IWebBrowser2 web in win)
+            {
+                if (Path.GetFileName(web.FullName).ToUpper() == "EXPLORER.EXE")
+                {
+                    string str = web.LocationURL;
+                    int cnt = listView1.Items.Find(str, false).Length;
+                    if (cnt > 0)
+                    {
+
+                    }
+                    else
+                    {
+
+                    }
+                    int idx = comboBox1.Items.IndexOf(str);
+                    if (idx < 0)
+                    {
+                        comboBox1.Items.Insert(0, str);
+                        string val = "1";
+                        string[] newitem = { str, val };
+                        listView1.Items.Add(new ListViewItem(newitem));
+                        paths += str;
+                    }
+                }
+            }
+            if (!paths.Equals(m_curpaths))
+            {
+                // update
+                label1.Text = "Updated";
+                m_curpaths = paths;
+            }
+            else
+            {
+                label1.Text = "Skip";
+
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+
+            Shell shell = new Shell();
+            ShellWindows win = shell.Windows();
+            foreach (IWebBrowser2 web in win)
+            {
+                if (Path.GetFileName(web.FullName).ToUpper() == "EXPLORER.EXE")
+                {
+                    label1.Text += web.LocationURL + " : " + web.LocationName + "\n";
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("EXPLORER.EXE", comboBox1.Text);
+        }
+
+        private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            //ListViewItem target = (ListViewItem)sender;
+            string seltext = "";
+            //seltext = target.SubItems[0].Text;
+            seltext = listView1.SelectedItems[0].SubItems[0].Text;
+            int val = int.Parse(listView1.SelectedItems[0].SubItems[1].Text);
+            val++;
+            listView1.SelectedItems[0].SubItems[1].Text = val.ToString();
+            System.Diagnostics.Process.Start("EXPLORER.EXE", seltext);
+        }
+    }
+}
